0315baef0e8e9f29b2582b752ffdae1f0f8b00fb
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2019-09-16 18:57:53 -0400

    I wrote 4 tests.6 to go

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Controls from './Controls';
+
+test('Dashboard renders correctly', () => {
+  render(<Controls />);
+});
+
+test('shows controls', () => {
+    // Arrange
+    const { getByText } = render(<Controls />);
+    // Act - getting the node by text
+    getByText(/lock gate/i);
+    getByText(/close gate/i);
+    // Assertion is if ^^^ is truthy
+  });
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
 // Test away
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+
+import Dashboard from './Dashboard';
+
+test('Dashboard renders correctly', () => {
+  render(<Dashboard />);
+});
+
+test('shows controls', () => {
+    // Arrange
+    const { getByText } = render(<Dashboard />);
+    // Act - getting the node by text
+    getByText(/lock gate/i);
+    getByText(/close gate/i);
+    // Assertion is if ^^^ is truthy
+  });
+
